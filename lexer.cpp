952c940766a24475e63732e2e5803ebd91769d33
tsaist1 <shengtsetsai@gmail.com>
2023-05-29 16:27:35 -0700

    +

--- lexer.cpp
+++ lexer.cpp
@@ -2,6 +2,8 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string>
+#include <memory>
+#include <vector>
 
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one of
 // these for known things.
@@ -21,11 +23,12 @@ enum Token
 static std::string identifier_string; // Filled in if tok_identifier
 static double num_val;                // Filled in if tok_number
 
+/// gettok - Return the next token from standard input.
 static int gettok()
 {
     static int last_char = ' ';
     // Skips any whitespace
-    while (isspace(last_char))
+    while (isspace(last_char)) // identifier: [a-zA-Z][a-zA-Z0-9]*
         last_char = getchar();
 
     if (isalpha(last_char)) {
@@ -41,7 +44,7 @@ static int gettok()
         return tok_identifier;
     }
 
-    if (isdigit(last_char) || last_char == '.') {
+    if (isdigit(last_char) || last_char == '.') {  // Number: [0-9.]+
         std::string num_str;
         do {
             num_str += last_char;
@@ -52,6 +55,7 @@ static int gettok()
     }
 
     if (last_char == '#') {
+        // Comment until end of line.
         do
             last_char = getchar();
         while (last_char != EOF && last_char != '\n' && last_char != '\r');
@@ -59,22 +63,76 @@ static int gettok()
         if (last_char != EOF)
             return gettok();
     }
-
+    // Check for end of file.  Don't eat the EOF.
     if (last_char == EOF)
         return tok_eof;
-
+    
+    // Otherwise, just return the character as its ascii value.
     int this_char = last_char;
     last_char = getchar();
     return this_char;
 }
 
+/// ExprAST - Base class for all expression nodes.
 class ExprAST {
 public:
     virtual ~ExprAST() = default;
 };
 
+/// NumberExprAST - Expression class for numeric literals like "1.0".
 class NumberExprAST : public ExprAST {
     double Val;
 public:
     NumberExprAST(double val) : Val(val) {}
+};
+
+/// VariableExprAST - Expression class for referencing a variable, like "a".
+class VariableExprAST :public ExprAST {
+    std::string Name;
+public:
+    VariableExprAST(const std::string &name) : Name(name) {}
+};
+
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+    char Op;
+    std::unique_ptr<ExprAST> LHS, RHS;
+public:
+    BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
+                  std::unique_ptr<ExprAST> RHS)
+        : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+    std::string Callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
+public:
+    CallExprAST(const std::string &Callee,
+                std::vector<std::unique_ptr<ExprAST>> Args)
+        : Callee(Callee), Args(std::move(Args)) {}
+};
+
+/// PrototypeAST - This class represents the "prototype" for a function,
+/// which captures its name, and its argument names (thus implicitly the number
+/// of arguments the function takes).
+class PrototypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+public:
+    PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+        : Name(Name), Args(std::move(Args)) {}
+
+    const std::string &getName() { return Name; }
+};
+
+/// FunctionAST - This class represents a function definition itself.
+class FunctionalAST {
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public:
+    FunctionalAST(std::unique_ptr<PrototypeAST> Proto,
+                  std::unique_ptr<ExprAST> Body)
+        : Proto(std::move(Proto)), Body(std::move(Body)) {}
 };
