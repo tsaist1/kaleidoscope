a65e00984fdc3cf9f9dfdbda21f6a9944bc9c840
tsaist1 <shengtsetsai@gmail.com>
2023-05-30 01:34:46 -0700

    +

--- lexer.cpp
+++ lexer.cpp
@@ -148,3 +148,22 @@ static int CurTok;
 static int getNextToken() {
     return CurTok = gettok();
 }
+
+std::unique_ptr<ExprAST> LogError(const char *Str)
+{
+    fprintf(stderr, "Error, %s\n", Str);
+    return nullptr;
+}
+std::unique_ptr<PrototypeAST> LogErrorP(const char *Str)
+{
+    LogError(Str);
+    return nullptr;
+}
+
+/// numberexp ::= number
+static std::unique_ptr<ExprAST> ParseNumberExpr()
+{
+    auto Result = std::make_unique<NumberExprAST>(NumVal);
+    getNextToken();
+    return std::move(Result);
+}
